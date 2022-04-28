34e3f22e6af141a28842eb4041e6d4498d92d3cd
Roberto Himmelbauer <robertoh89@gmail.com>
2022-04-28 13:34:41 -0700

    update setting.py to include active campaign

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-    'django-active-campaign',
+    'django_active_campaign',
 ]
 
 MIDDLEWARE = [
